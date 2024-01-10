8354b07c56f4980b261deef7ef233408c72a61bc
Satish Radadiya <satishradadiya@gmail.com>
2024-01-10 03:19:31 +0100

    function learning

---
+++ function/func.go
@@ -0,0 +1,18 @@
+//  syntax  example
+
+// func funcName(){
+
+//}
+
+package main
+
+import "fmt"
+
+// Print function in the function
+func main() {
+	printString()
+}
+
+func printString() {
+	fmt.Println("Hello world")
+}
