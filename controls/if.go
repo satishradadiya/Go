3d9b66277d7e3dd8183d13e797d43e8c74f06424
Satish Radadiya <satishradadiya@gmail.com>
2024-01-15 04:47:44 +0100

    few more if else learning

--- controls/if.go
+++ controls/if.go
@@ -11,5 +11,12 @@ func main() {
 	if 8/2 == 4 || 7%2 == 0 {
 		fmt.Println("One of the fiven condition matched")
 	}
-
+	// var num int = 9
+	if num := 9; num < 3 {
+		fmt.Println(num, "is negative")
+	} else if num < 11 {
+		fmt.Println(num, "is positive")
+	} else {
+		fmt.Println(num, "is something")
+	}
 }
